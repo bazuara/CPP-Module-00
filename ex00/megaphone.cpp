8f7dca1b0678df4798d3e1575dd00ec179f17e6b
Bruno Azuara <bruno.azuara@gmail.com>
2023-03-02 16:05:11 +0100

    replace whiles with fors

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: brunoazuara <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 00:45:53 by brunoazu          #+#    #+#             */
-/*   Updated: 2023/03/02 15:50:10 by brunoazu         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:04:29 by brunoazu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 
 namespace 
 {
-void printUpper (const std::string &str)
-{
-	int i = 0;
-	while(str[i]) {
-		std::cout << (char)toupper(str[i]); //Cambiar a uso de strings en lugar de char *
-		i++;
+	void printUpper (const std::string &str)
+	{
+		for (int i = 0; str[i]; i++) {
+			std::cout << (char)toupper(str[i]);
+		}
 	}
-}
-} //namespace end
+} //end namespace
 
 int main (int argc, char *argv[])
 {
