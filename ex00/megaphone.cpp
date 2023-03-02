e14d9f7bd5a4d58a73b09ea02b0fb193a97c390b
Bruno Azuara <bruno.azuara@gmail.com>
2023-03-02 13:38:15 +0100

    change from char * to string

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,29 +6,35 @@
 /*   By: brunoazuara <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 00:45:53 by brunoazu          #+#    #+#             */
-/*   Updated: 2022/12/24 11:58:49 by brunoazu         ###   ########.fr       */
+/*   Updated: 2023/03/01 11:41:45 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <string>
 
-void printUpper (char *str)
+// meter en un propio namespace 
+namespace 
+{
+	
+void printUpper (const std::string &str)
 {
 	int i = 0;
 	while(str[i]) {
-		std::cout << (char)toupper(str[i]);
+		std::cout << (char)toupper(str[i]); //Cambiar a uso de strings en lugar de char *
 		i++;
 	}
 }
+} //namespace end
 
 int main (int argc, char *argv[])
 {
 	int i;
 
 	if (argc == 1)
-		printUpper((char *)"* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+		printUpper("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
 	else {
 		i = 1;
 		while (i < argc) {
@@ -36,6 +42,7 @@ int main (int argc, char *argv[])
 			i++;
 		}
 	}
+	// mejor con nn para no vaciar el buffer (no lo tengo muy claro)
 	std::cout << std::endl;
 	return 0;
 }
