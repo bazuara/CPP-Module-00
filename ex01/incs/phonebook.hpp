5f3462d11b33b9d289f68f2004a798974bb2c9f3
Bruno Azuara <bruno.azuara@gmail.com>
2023-03-02 16:23:26 +0100

    remove useless comment

--- ex01/incs/phonebook.hpp
+++ ex01/incs/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: brunoazuara <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 21:44:45 by brunoazu          #+#    #+#             */
-/*   Updated: 2023/03/01 11:43:24 by bazuara          ###   ########.fr       */
+/*   Updated: 2023/03/02 16:22:32 by brunoazu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include <contact.hpp>
 
 struct PhoneBook {
-	// primero las publicas y primero constuctor
 		PhoneBook();
 
 		void 	addContact();
