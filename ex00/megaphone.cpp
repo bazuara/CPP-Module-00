ff1d9349997282c8bf3916067c451eb6eb13ab62
Bruno Azuara <bruno.azuara@gmail.com>
2023-03-02 16:21:03 +0100

    change counter scope

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: brunoazuara <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 00:45:53 by brunoazu          #+#    #+#             */
-/*   Updated: 2023/03/02 16:04:29 by brunoazu         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:20:32 by brunoazu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,11 @@ namespace
 
 int main (int argc, char *argv[])
 {
-	int i;
 
 	if (argc == 1)
 		printUpper("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
 	else {
-		i = 1;
+		int i = 1;
 		while (i < argc) {
 			printUpper(argv[i]);
 			i++;
